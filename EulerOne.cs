d17a2d11c2a031739a54ac2ee6dd293e76bdf2d9
Azadi Bogolubov <azadibogolubov@gmail.com>
2013-11-20 18:32:39 -0800

    Solutions for #1, 3, 5, 9 in C#

---
+++ EulerOne.cs
@@ -0,0 +1,24 @@
+﻿// Project Euler Problem #1
+// Calculate the sum of the multiples of 3 and 5 below 1000 exclusive.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class EulerOne
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                    sum += i;
+            }
+            Console.WriteLine("The sum of the multiples of 3 and 5 below 1000 are: " + sum);
+        }
+    }
+}
---
+++ EulerThree.cs
@@ -0,0 +1,53 @@
+﻿// Project Euler Problem 3
+// Solution to: What is the largest prime factor of the number 600851475143?
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prok2ectEulerOne
+{
+    class EulerThree
+    {
+        static void Main(string[] args)
+        {
+            const long EULERNUMBER = 600851475143;
+            long largest_factor = 0;
+            long[] factors = new long[2];
+
+            for (long i = 2; i * i < EULERNUMBER; i++)
+            {
+                if (EULERNUMBER % i == 0)
+                { 
+                    // We found a factor of the given number.
+                    factors[0] = i;
+                    // Let's find the other factor.
+                    factors[1] = EULERNUMBER / i;
+
+                    // Now find if either are prime...
+                    for (int j = 0; j < 2; j++)
+                    {
+                        bool isPrime = true;
+                        for (long k = 2; k * k < factors[j]; k++)
+                        {
+                            if (factors[j] % k == 0)
+                            {
+                                isPrime = false;
+                                break;
+                            }
+                        }
+
+                        // Is this factor larger than the previous?
+                        if (isPrime && factors[j] > largest_factor)
+                        {
+                            largest_factor = factors[j];
+                        }
+                    }
+                }
+            }
+            Console.Write("The largest prime factor of " + EULERNUMBER + " is: " + largest_factor);
+            Console.ReadLine();
+        }
+    }
+}
